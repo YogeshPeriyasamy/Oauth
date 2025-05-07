b877bcbfd8d7cd2ac9349ffdc252b96b26451d9d
Yogeshperiyasamy <yogeshsri1209@gmail.com>
2025-05-07 21:46:02 +0530

    added facebook oauth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 
+//for google
 exports.googleLogin = (req, res) => {
-    console.log("first login")
+    //requesting code from google
+  console.log("first login")
   const redirectUri = process.env.GOOGLE_REDIRECT_URI;
   const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=email%20profile`;
 
@@ -9,6 +11,7 @@ exports.googleLogin = (req, res) => {
 };
 
 exports.googleCallback = async (req, res) => {
+    //exchanging code for token
   const code = req.query.code;
  console.log("got code",code)
   const tokenRes = await axios.post("https://oauth2.googleapis.com/token", {
@@ -20,7 +23,7 @@ exports.googleCallback = async (req, res) => {
   });
 
   const accessToken = tokenRes.data.access_token;
-
+//using token to get the user info
   const userInfo = await axios.get("https://www.googleapis.com/oauth2/v2/userinfo", {
     headers: { Authorization: `Bearer ${accessToken}` },
   });
@@ -31,3 +34,39 @@ exports.googleCallback = async (req, res) => {
  
   res.send("Google Login Success");
 };
+
+//for facebook
+exports.facebookLogin = (req, res) => {
+    //requesting facebook for code
+    const redirectUri = process.env.FACEBOOK_REDIRECT_URI;
+    const authUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${redirectUri}&scope=email`;
+  
+    res.redirect(authUrl);
+  };
+  
+  exports.facebookCallback = async (req, res) => {
+    const code = req.query.code;
+    //exchanging code for token
+    const tokenRes = await axios.get("https://graph.facebook.com/v18.0/oauth/access_token", {
+      params: {
+        client_id: process.env.FACEBOOK_APP_ID,
+        client_secret: process.env.FACEBOOK_APP_SECRET,
+        redirect_uri: process.env.FACEBOOK_REDIRECT_URI,
+        code,
+      },
+    });
+  
+    const accessToken = tokenRes.data.access_token;
+    //using token to enquire user info
+    const userInfo = await axios.get(`https://graph.facebook.com/me`, {
+      params: {
+        access_token: accessToken,
+        fields: "id,name,email,picture",
+      },
+    });
+  
+    const user = userInfo.data; // id, name, email
+    console.log("Facebook User:", user);
+  
+    res.send("Facebook Login Success");
+  };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {googleLogin, googleCallback,} = require("../controllers/authController");
+const {googleLogin, googleCallback,facebookLogin,facebookCallback} = require("../controllers/authController");
 
+//route for google Oauth
 router.get("/auth/google", googleLogin);
 router.get("/auth/google/callback", googleCallback);
 
+//route for facebook Oauth
+router.get("/auth/facebook", facebookLogin);
+router.get("/auth/facebook/callback", facebookCallback);
 
 module.exports = router;
